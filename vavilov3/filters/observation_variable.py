114eb65eef48d4557f130352b3fcbf787b71c534
peio <pziarsolo@upv.es>
2019-09-18 12:14:07 +0200

    add new filter for observation variable

--- vavilov3/filters/observation_variable.py
+++ vavilov3/filters/observation_variable.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django_filters import rest_framework as filters
 
 from vavilov3.models import ObservationVariable
@@ -11,6 +12,8 @@ class ObservationVariableFilter(TermFilterMixin, filters.FilterSet):
                                distinct=True)
     study = filters.CharFilter(field_name='observation__observation_unit__study__name',
                                lookup_expr='iexact', distinct=True)
+    name_or_desc = filters.CharFilter(label='term', method='name_or_desc_filter',
+                                      distinct=True)
 
     class Meta:
         model = ObservationVariable
@@ -19,3 +22,7 @@ class ObservationVariableFilter(TermFilterMixin, filters.FilterSet):
                   'trait__name': ['exact', 'iexact', 'icontains'],
                   'method': ['exact', 'iexact', 'icontains'],
                   'scale__name': ['exact', 'iexact', 'icontains'], }
+
+    def name_or_desc_filter(self, queryset, _, value):
+        return queryset.filter(Q(name__icontains=value) |
+                               Q(description__icontains=value))
